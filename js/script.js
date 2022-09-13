0f9501075eacb36464aaa475e7c2ca29403212b1
McFlyZero <arturoo_dh@live.com>
2022-09-13 13:37:34 -0700

    Subiendo proyecto

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*=============================================
+=                     BANNER                  =
+=============================================*/
+$(".fade-slider").jdSlider({
+	isSliding: false,
+	isAuto: true,
+	isLoop: true,
+	isDrag: false,
+	interval: 5000,
+	isCursor: false,
+	speed: 3000
+
+})
