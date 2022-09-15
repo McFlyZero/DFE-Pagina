0095c5c7b9a397d6058ef8d05c07f6824d497ff3
McFlyZero <arturoo_dh@live.com>
2022-09-14 22:27:13 -0700

    Banner estatico y color invertido

--- js/script.js
+++ js/script.js
@@ -10,4 +10,23 @@ $(".fade-slider").jdSlider({
 	isCursor: false,
 	speed: 3000
 
-})
+});
+
+var alturaBanner = $(".fade-slider").height();
+console.log("alturaBanner",alturaBanner);
+$(".bannerEstatico").css({"height":alturaBanner+"px"})
+
+/*================================(
+ANIMACIONES SCROLL
+================================*/
+$(window).scroll(function() {
+	var posY = window.pageYOffset;
+
+	if (posY > alturaBanner){
+		$("header").css({"background":"white"})
+		$("header .logotipo").css({"filter":"invert(100%)"})
+	}else{
+		$("header").css({"background":"rgba(0,0,0,.5)"})
+		$("header .logotipo").css({"filter":"invert(0%)"})
+	}
+});
