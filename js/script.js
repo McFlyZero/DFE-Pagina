ad069722b0b3421fae8c240872abcebce9927a95
McFlyZero <arturoo_dh@live.com>
2022-10-13 13:33:29 -0700

    Preload de la pagina (css), imagen loader y javascript para cargar Signed-off-by: McFlyZero <arturoo_dh@live.com>

--- js/script.js
+++ js/script.js
@@ -72,8 +72,7 @@ var controller = $.superscrollorama();
 
 controller.addTween(".contenidoInicio .container", TweenMax.from(
 		$(".contenidoInicio .container"), .5, {css:{opacity: 0}}
-	))
-
+))
 /*==============================================
 SCROLLUP
 ==============================================*/
@@ -83,3 +82,51 @@ $.scrollUp({
 	scrollSpeed: 2000,
 	easingType: "easeOutQuint"
 })
+/*==============================================
+PRELOAD
+==============================================*/
+$("body").css({"overflow-y":"hidden"})
+
+var cargarImg = $("img");
+var cargarScript = $("script");
+var cargarCSS = $("link");
+var cargarVideos = $("video");
+var cargarAudios = $("audio");
+var totalObjetos = [];
+var numItem = 0;
+var valorPorcentaje = 0;
+var incremento = 0;
+var numCarga = 0;
+
+totalObjetos.push(cargarImg,cargarScript,cargarCSS,cargarVideos,cargarAudios);
+totalObjetos.forEach(funcionForEach);
+
+function funcionForEach(item, index){
+
+	for (var i = 0; i < item.length; i++) {
+
+		numItem++;
+
+		valorPorcentaje = 100/numItem;
+
+	}
+
+	for (var i = 0; i < item.length; i++) {
+		
+		$(item[i]).ready(function(){
+
+			numCarga++;
+
+			incremento = Math.ceil(numCarga * valorPorcentaje);
+			$("#porcentajeCarga").html(incremento+"%");
+
+			$("#rellenoCarga").css({"width":incremento+"%"});
+
+			if(incremento >= 100){
+
+				$("#preload").delay(350).fadeOut("slow");
+				$("#body").delay(350).css({"overflow-y":"scroll"});
+			}
+		})
+	}
+}
